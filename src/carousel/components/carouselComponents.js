d1f85387e45ad06e21a0d11174d7746b8baa497d
Dylan Palmboom <nofxboy1234@gmail.com>
2024-04-26 15:08:21 +0200

    Create modules for carousel components and logic

---
+++ src/carousel/components/carouselComponents.js
@@ -0,0 +1,15 @@
+const pictureFrame = 'pictureFrame';
+const slideShow = 'slideShow';
+const slide = 'slide';
+const previousButton = 'previousButton';
+const nextButton = 'nextButton';
+const jumpCircle = 'jumpCircle';
+
+export {
+  pictureFrame,
+  slideShow,
+  slide,
+  previousButton,
+  nextButton,
+  jumpCircle,
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import './style.css';
 
 import setupMenus from './dropDownMenu';
+import {
+  pictureFrame,
+  slideShow,
+  slide,
+  previousButton,
+  nextButton,
+  jumpCircle,
+} from './carousel/components/carouselComponents';
 
 setupMenus();
