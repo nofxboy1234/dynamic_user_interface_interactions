2f7db4623e0ec0fc297c0b99eb492b4e08a2a7b0
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-02 12:20:52 +0200

    Refactor carousel effect

--- src/carousel/logic/carouselLogic.js
+++ src/carousel/logic/carouselLogic.js
@@ -4,47 +4,38 @@ const minIndex = 0;
 let slideIndex = 0;
 const translateX = [0, -256 * 1, -256 * 2, -256 * 3, -256 * 4];
 
-let slideShowLeft = document.querySelector('.slide-show');
-let slideShowRight;
-const slideShowContainer = document.querySelector('.slide-show-container');
+const slideShowContainer = document.querySelector('#slide-show-container');
+
+const slideShowLeft = document.querySelector('#slide-show-left');
+const slideShowRight = document.querySelector('#slide-show-right');
+const lastSlideShowAppended = slideShowRight;
+
+const nextSlideShow = function nextSlideShow() {
+  let slideShow;
+  switch (lastSlideShowAppended) {
+    case slideShowLeft:
+      slideShow = slideShowRight;
+      break;
+    case slideShowRight:
+      slideShow = slideShowLeft;
+      break;
+    default:
+      break;
+  }
+
+  return slideShow;
+};
 
 slideShowContainer.addEventListener('transitionend', () => {
-  if (slideIndex <= maxIndex) {
-    return;
+  if (slideIndex > maxIndex) {
+    slideShowContainer.appendChild(nextSlideShow());
+    slideIndex = minIndex;
   }
-
-  slideShowLeft.remove();
-  slideShowLeft = slideShowRight;
-  slideIndex = minIndex;
 });
-// }, {capture: true});
-
-const previous = function previous() {
-  slideIndex -= 1;
-
-  if (slideIndex < minIndex) {
-    slideIndex = maxIndex;
-  }
-
-  slideShowLeft.style.transform = `translateX(${translateX.at(slideIndex)}px)`;
-};
 
 const next = function next() {
   slideIndex += 1;
-
-  if (slideIndex > maxIndex) {
-    //   Create a copy of slideshow and put it to the right of existing slideshow
-    slideShowRight = slideShowLeft.cloneNode(true);
-    slideShowRight.id = 'slide-show-right';
-    slideShowLeft.after(slideShowRight);
-  }
-
   slideShowContainer.style.translate = `${translateX.at(slideIndex)}px`;
-};
-
-const jump = function jump(index) {
-  slideIndex = index;
-  slideShowLeft.style.transform = `translateX(${translateX.at(slideIndex)}px)`;
 };
 
 const setAutoAdvance = function setAutoAdvance(delay) {
@@ -55,4 +46,19 @@ const setAutoAdvance = function setAutoAdvance(delay) {
   return intervalID;
 };
 
-export { previous, next, jump, setAutoAdvance };
+// const previous = function previous() {
+//   slideIndex -= 1;
+
+//   if (slideIndex < minIndex) {
+//     slideIndex = maxIndex;
+//   }
+
+//   slideShowLeft.style.transform = `translateX(${translateX.at(slideIndex)}px)`;
+// };
+
+// const jump = function jump(index) {
+//   slideIndex = index;
+//   slideShowLeft.style.transform = `translateX(${translateX.at(slideIndex)}px)`;
+// };
+
+export { next, setAutoAdvance };
--- src/index.js
+++ src/index.js
@@ -1,55 +1,29 @@
 import './style.css';
 
+import { next, setAutoAdvance } from './carousel/logic/carouselLogic';
+
 // import setupMenus from './dropDownMenu';
 
-// import {
-//   pictureFrame,
-//   slideShow,
-//   slide,
-//   previousButton,
-//   nextButton,
-//   jumpCircle,
-// } from './carousel/components/carouselComponents';
-
-// import {
-//   previous,
-//   next,
-//   jump,
-//   setAutoAdvance,
-// } from './carousel/logic/carouselLogic';
-
 // setupMenus();
-// setAutoAdvance(1000);
 
-const outer = document.querySelector('#outer');
-// outer.addEventListener('click', log);
-outer.addEventListener('click', log, { capture: true });
+setAutoAdvance(1000);
 
-const inner1 = document.querySelector('#inner-1');
-inner1.addEventListener('click', log);
+// setTimeout(() => {
+//   next();
+// }, 1000);
 
-const inner2 = document.querySelector('#inner-2');
-const inner3 = document.querySelector('#inner-3');
+// setTimeout(() => {
+//   next();
+// }, 2000);
 
-function log(event) {
-  console.log('\n');
-  console.log('**************');
-  console.log('event.target:');
-  console.log(event.target);
-  console.log('##############');
-  console.log('event.currentTarget:');
-  console.log(event.currentTarget);
-  console.log('##############');
-  console.log('this:');
-  console.log(this);
-  console.log('**************');
+// setTimeout(() => {
+//   next();
+// }, 3000);
 
-  // if (this === outer) {
-  //   event.stopPropagation();
-  // }
-  // event.stopPropagation();
-}
+// setTimeout(() => {
+//   next();
+// }, 4000);
 
-function log2(event) {
-  console.log('log2!');
-}
+// setTimeout(() => {
+//   next();
+// }, 5000);
