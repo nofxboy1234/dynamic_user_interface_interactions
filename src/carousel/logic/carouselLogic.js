0b4e53e2735e51d8ef5b1150d9a2fb187d42017c
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-02 14:50:35 +0200

    Simplify slideshow element structure

--- src/carousel/logic/carouselLogic.js
+++ src/carousel/logic/carouselLogic.js
@@ -1,33 +1,26 @@
-const maxIndex = 3;
 const minIndex = 0;
+const maxIndex = 3;
 
 let slideIndex = 0;
 // const translateX = [0, -256 * 1, -256 * 2, -256 * 3, -256 * 4];
 const translateDistance = 256;
 
-const slideShowContainer = document.querySelector('#slide-show-container');
+const slideShow = document.querySelector('#slide-show');
 
-const slideShow1 = document.querySelector('#slide-show-1');
-const slideShow2 = document.querySelector('#slide-show-2');
-const slideShow3 = document.querySelector('#slide-show-3');
-const lastSlideShowAppended = slideShow2;
-
-slideShowContainer.addEventListener('transitionend', () => {
+slideShow.addEventListener('transitionend', () => {
   console.log('transitionend');
 
-  if (slideIndex === maxIndex + 1) {
-    // const firstSlide = slideShowContainer.firstChild;
-    // slideShowContainer.removeChild(firstSlide);
-    // slideShowContainer.appendChild(firstSlide);
-    // slideShowContainer.lastChild.after(slideShowContainer.firstChild)
-    // slideIndex = minIndex;
-  }
+  // const firstSlide = slideShowContainer.firstChild;
+  // slideShowContainer.removeChild(firstSlide);
+  // slideShowContainer.appendChild(firstSlide);
+  // slideShowContainer.lastChild.after(slideShowContainer.firstChild)
+  // slideIndex = minIndex;
 });
 
 const next = function next() {
   slideIndex += 1;
   const translateValue = `${-1 * slideIndex * translateDistance}px`;
-  slideShowContainer.style.translate = translateValue;
+  slideShow.style.translate = translateValue;
 };
 
 const setAutoAdvance = function setAutoAdvance(delay) {
